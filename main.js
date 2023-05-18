fa61370832af5612ccbaf2da6db3e41c9bc4530d
sf.safayet36@gmail.com <sf.safayet36@gmail.com>
2023-05-18 11:34:54 +0600

    Nav update

---
+++ main.js
@@ -0,0 +1,11 @@
+let navtoggle = document.querySelector(".navtoggle")
+
+let sidebar = document.querySelector(".ullist"
+)
+
+navtoggle.addEventListener("click", function(){
+  sidebar.classList.toggle('open')
+  
+})
+
+
